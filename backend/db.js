05f93d72fcf64cbdd04c327cdb60625c7a5711e9
Bruno Aseff <brunoaseff2@gmail.com>
2025-06-26 10:25:07 -0300

    feat: update server to modules

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,20 @@
-const mysql = require('mysql2');
-require('dotenv').config();
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+ host: process.env.DB_HOST,
+ port: process.env.DB_PORT,
+ user: process.env.DB_USER,
+ password: process.env.DB_PASSWORD,
+ database: process.env.DB_NAME,
+ waitForConnections: true,
+ connectionLimit: 10,
+ queueLimit: 0,
 });
 
-module.exports = pool.promise();
+
+export default pool.promise();
