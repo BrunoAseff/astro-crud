11cb9acc0cf18b627d82492cd7f3b140586645b3
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-06-30 14:29:46 +0000

    feat: add update routes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import axios from 'axios';
 
 const api = axios.create({
@@ -12,6 +14,9 @@ export const insertData = () => api.post('/setup/insert-data');
 export const getGalaxiesCount = () => api.get('/galaxies/count');
 export const getAllGalaxies = () => api.get('/galaxies');
 export const getGalaxiesWithStats = () => api.get('/galaxies/stats');
+export const createGalaxy = (data: any) => api.post('/galaxies', data);
+export const updateGalaxy = (id: number, data: any) =>
+  api.put(`/galaxies/${id}`, data);
 export const deleteGalaxy = (id: number) => api.delete(`/galaxies/${id}`);
 
 // Planets
@@ -19,6 +24,9 @@ export const getPlanetsCount = () => api.get('/planets/count');
 export const getAllPlanets = () => api.get('/planets/with-galaxy');
 export const getHabitablePlanetsCount = () =>
   api.get('/planets/habitable').then((res) => ({ total: res.data.length }));
+export const createPlanet = (data: any) => api.post('/planets', data);
+export const updatePlanet = (id: number, data: any) =>
+  api.put(`/planets/${id}`, data);
 export const deletePlanet = (id: number) => api.delete(`/planets/${id}`);
 
 // Stars
@@ -26,6 +34,9 @@ export const getStarsCount = () => api.get('/stars/count');
 export const getAllStars = () => api.get('/stars/with-galaxy');
 export const getBrightestStar = () =>
   api.get('/stars/brightest').then((res) => res.data[0]);
+export const createStar = (data: any) => api.post('/stars', data);
+export const updateStar = (id: number, data: any) =>
+  api.put(`/stars/${id}`, data);
 export const deleteStar = (id: number) => api.delete(`/stars/${id}`);
 
 // Elements
@@ -33,6 +44,9 @@ export const getElementsCount = () => api.get('/elements/count');
 export const getAllElements = () => api.get('/elements/with-planets');
 export const getMostCommonElement = () =>
   api.get('/elements/most-common').then((res) => res.data[0]);
+export const createElement = (data: any) => api.post('/elements', data);
+export const updateElement = (id: number, data: any) =>
+  api.put(`/elements/${id}`, data);
 export const deleteElement = (id: number) => api.delete(`/elements/${id}`);
 
 // Clusters
@@ -40,6 +54,9 @@ export const getClustersCount = () => api.get('/clusters/count');
 export const getAllClusters = () => api.get('/clusters/with-galaxy');
 export const getClosestCluster = () =>
   api.get('/clusters/closest').then((res) => res.data[0]);
+export const createCluster = (data: any) => api.post('/clusters', data);
+export const updateCluster = (id: number, data: any) =>
+  api.put(`/clusters/${id}`, data);
 export const deleteCluster = (id: number) => api.delete(`/clusters/${id}`);
 
 // Nebulae
@@ -47,6 +64,9 @@ export const getNebulaeCount = () => api.get('/nebulae/count');
 export const getAllNebulae = () => api.get('/nebulae/with-galaxy');
 export const getClosestNebula = () =>
   api.get('/nebulae/closest').then((res) => res.data[0]);
+export const createNebula = (data: any) => api.post('/nebulae', data);
+export const updateNebula = (id: number, data: any) =>
+  api.put(`/nebulae/${id}`, data);
 export const deleteNebula = (id: number) => api.delete(`/nebulae/${id}`);
 
 // Exoplanets
@@ -56,4 +76,7 @@ export const getLargestExoplanet = () =>
   api.get('/exoplanets/largest').then((res) => res.data[0]);
 export const getMostMassiveExoplanet = () =>
   api.get('/exoplanets/most-massive').then((res) => res.data[0]);
+export const createExoplanet = (data: any) => api.post('/exoplanets', data);
+export const updateExoplanet = (id: number, data: any) =>
+  api.put(`/exoplanets/${id}`, data);
 export const deleteExoplanet = (id: number) => api.delete(`/exoplanets/${id}`);
